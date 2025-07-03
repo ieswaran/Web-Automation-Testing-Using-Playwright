d542e5f38e370bb4def39e2de4a948c59238cce8
Eswaran Anantharaman <69461475+ieswaran@users.noreply.github.com>
2025-07-03 11:04:06 +0530

    Update FlipkartTests.spec.ts

--- tests/week2HomeAssignments/FlipkartTests.spec.ts
+++ tests/week2HomeAssignments/FlipkartTests.spec.ts
@@ -1,37 +1,3 @@
-/* 
-Home Assignment <2.2>
-Red Bus and Flipkart in Edge and Firefox Browser Instances (Week2.Day2)
-Learning Objective:
- Learn how to automate basic navigation to two different websites (Red Bus and Flipkart)
-in two separate browser instances (Edge and Firefox) using Playwright.
-Expected Completion Time:
- Best Case: 15 minutes
- Average Case: 20 minutes
-Assignment Details:
-Task:
-Your task is to launch two separate browser instances using Playwright:
-1. 2. Load Red Bus in an Edge browser instance and verify the page title and URL.
-Load Flipkart in a Firefox browser instance and verify the page title and URL.
-Preconditions:
-1. 2. Use Playwright to launch Edge and Firefox.
-Create two separate browser instances.
-3. Use the following URLs:
-o Red Bus: https://www.redbus.in
-o Flipkart: https://www.flipkart.com
-Requirements:
- Red Bus (Edge):
- Flipkart (Firefox):
-o Load the home page and print the page title and current URL.
-o Load the home page and print the page title and current URL.
-Hints:
- Use page.title() to retrieve the page title.
- Use page.url() to retrieve the current page URL.
-Expected Outcome:
- Upon completion, you should be able to navigate two websites in two separate browser
-instances (Edge and Firefox), retrieve page information, and print the page titles and
-URLs.
-
-*/
 import { test, expect, chromium, firefox } from "@playwright/test";
 
 test.describe("Red Bus and Flipkart Navigation Tests", () => {
